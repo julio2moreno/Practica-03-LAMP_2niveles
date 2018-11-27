5f6e288b6c280bbe986b3e2a7735d810c12d0b74
julio2moreno <43573057+julio2moreno@users.noreply.github.com>
2018-11-27 21:25:50 +0100

    Add files via upload

---
+++ provision-for-mysql.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+apt-get update
+apt-get -y install debconf-utils
+
+DB_ROOT_PASSWD=root
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DB_ROOT_PASSWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DB_ROOT_PASSWD"
+
+apt-get install -y mysql-server
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+/etc/init.d/mysql restart
+
+apt-get install -y git
+cd /tmp
+rm -rf iaw-practica-lamp
+git clone https://github.com/josejuansanchez/iaw-practica-lamp.git
+
+mysql -u root mysql -p$DB_ROOT_PASSWD <<< "GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY '$DB_ROOT_PASSWD'; FLUSH PRIVILEGES;"
